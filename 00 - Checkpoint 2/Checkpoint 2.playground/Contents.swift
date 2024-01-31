155d3c8956458f266e7964b429ccda209cfcf1b9
Luis Rivera Rivera <luisrockero11@hotmail.com>
2024-01-31 00:29:06 -0400

    Added Checkpoint 2

---
+++ 00 - Checkpoint 2/Checkpoint 2.playground/Contents.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+let arrayOfStrings: [String] = ["Apple", "iOS", "SwiftUI", "Hacking with Swift", "Apple"]
+
+print("The array of strings contains \(arrayOfStrings.count) elements")
+print("Meanwhile the same array contains \(Set(arrayOfStrings).count) unique items.")
