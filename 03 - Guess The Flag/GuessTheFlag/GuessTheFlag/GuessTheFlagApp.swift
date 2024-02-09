bad5bcfcbaf771455990964d661b21fb2b56cf11
Luis Rivera Rivera <luisrockero11@hotmail.com>
2024-02-08 22:40:25 -0400

    Added Guess the flag completed project

---
+++ 03 - Guess The Flag/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Luis Rivera Rivera on 2/8/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
