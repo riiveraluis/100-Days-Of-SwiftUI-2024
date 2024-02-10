5cd16c196ae5c7d4a1a14efe7f8dfdf6f363775c
Luis Rivera Rivera <luisrockero11@hotmail.com>
2024-02-10 14:38:12 -0400

    Added ViewsAndModifiers completed project

---
+++ 04 - ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Luis Rivera Rivera on 2/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
