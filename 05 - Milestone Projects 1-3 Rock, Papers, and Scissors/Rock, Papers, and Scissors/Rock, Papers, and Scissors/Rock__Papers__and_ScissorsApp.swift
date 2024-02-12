32dabe3cc301e0adcd91febadffe374105552d24
Luis Rivera Rivera <luisrockero11@hotmail.com>
2024-02-11 23:23:12 -0400

    05 - Milestone Projects 1-3 Rock, Papers, and Scissors initial

---
+++ 05 - Milestone Projects 1-3 Rock, Papers, and Scissors/Rock, Papers, and Scissors/Rock, Papers, and Scissors/Rock__Papers__and_ScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock__Papers__and_ScissorsApp.swift
+//  Rock, Papers, and Scissors
+//
+//  Created by Luis Rivera Rivera on 2/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Rock__Papers__and_ScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
