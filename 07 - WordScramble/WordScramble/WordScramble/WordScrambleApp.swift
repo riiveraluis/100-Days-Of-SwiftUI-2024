ec627c4cd6f5c24d5c5243036deb6fd35a0e0f3a
Luis Rivera Rivera <luisrockero11@hotmail.com>
2024-02-18 20:37:26 -0400

    Word Scramble Initial Project

---
+++ 07 - WordScramble/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Luis Rivera Rivera on 2/16/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
