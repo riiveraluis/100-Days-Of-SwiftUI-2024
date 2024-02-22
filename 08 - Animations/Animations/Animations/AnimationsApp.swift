ea3f8d13051c2c3a3c57a4c8121a0dd7ef25f273
Luis Rivera Rivera <luisrockero11@hotmail.com>
2024-02-21 22:11:47 -0400

    Completed Animations Technical Project

---
+++ 08 - Animations/Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Luis Rivera Rivera on 2/20/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
