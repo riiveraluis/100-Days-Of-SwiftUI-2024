178e4b51504a2ce137e78bfa11c1542c18c3e7fb
Luis Rivera Rivera <luisrockero11@hotmail.com>
2024-02-25 16:22:26 -0400

    Added iExpense project completed

---
+++ 10- iExpense/iExpense/iExpense/AddView.swift
@@ -0,0 +1,46 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Luis Rivera Rivera on 2/25/24.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @Environment (\.dismiss) var dissmis
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0.0
+    
+    let types = ["Personal", "Business"]
+    
+    var expenses: Expenses
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Name", text: $name)
+                Picker("Type", selection: $type) {
+                    ForEach(types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                TextField("Amount", value: $amount, format: .currency(code: "USD"))
+                    .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add new expense")
+            .toolbar {
+                Button("Save") {
+                    let item = ExpenseItem(name: name, type: type, amount: amount)
+                    expenses.items.insert(item, at: 0)
+                    dissmis()
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    AddView(expenses: Expenses())
+}
---
+++ 10- iExpense/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Luis Rivera Rivera on 2/25/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
